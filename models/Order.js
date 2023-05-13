dee6b7ce76247f88e39f9270df37fc24b8b3f0d6
sarnakhmel <OlSa@olsa.local>
2023-05-13 08:52:49 +0300

    OrderSchema

---
+++ models/Order.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const OrderSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+    },
+    userPhone: {
+      type: String,
+      required: true,
+    },
+    userEmail: {
+      type: String,
+      required: false,
+    },
+    userAddress: {
+      type: String,
+      required: true,
+    },
+    orderPrice: {
+      type: String,
+      required: true,
+    },
+    orderWeight: {
+      type: String,
+      required: true,
+    },
+    orderList: {
+      type: String,
+      required: false,
+    },
+    orderPromo: {
+      type: String,
+      required: false,
+    },
+    viewsCount: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    imageUrl: String,
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Order", OrderSchema);
